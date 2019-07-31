24aec32ab1071fe9bf20748941adc8ccdf84cf0e
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-07-31 14:14:53 -0400

    Primer deploy

---
+++ precache-manifest.a6b649e8ed4ad4c03e21858866df7461.js
@@ -0,0 +1,63 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcc6e6a4176545b66ab",
+    "url": "/0.app.bundle.js"
+  },
+  {
+    "revision": "6d241e4288da4f3ab3f4",
+    "url": "/1.app.bundle.js"
+  },
+  {
+    "revision": "0d1f5eb85ffd5d791e08",
+    "url": "/3.app.bundle.js"
+  },
+  {
+    "revision": "025912d9136e61d55014",
+    "url": "/4.app.bundle.js"
+  },
+  {
+    "revision": "f25eba16dc89a8058a47",
+    "url": "/5.app.bundle.js"
+  },
+  {
+    "revision": "b7327318d34cf17d8fc8",
+    "url": "/6.app.bundle.js"
+  },
+  {
+    "revision": "89bef6bd8701f2e33dc1",
+    "url": "/7.app.bundle.js"
+  },
+  {
+    "revision": "5210cf2595c5c7c1eb5e",
+    "url": "/8.app.bundle.js"
+  },
+  {
+    "revision": "742417953b9bc3466836",
+    "url": "/app.bundle.js"
+  },
+  {
+    "url": "/icon_128x128.1999903bb563f50d9af18423e7940be2.png"
+  },
+  {
+    "url": "/icon_192x192.c354a953eccf4e2cff5c98b306d55782.png"
+  },
+  {
+    "url": "/icon_256x256.78b99666e8f3471c32954c6206f8f427.png"
+  },
+  {
+    "url": "/icon_384x384.63d261e6918ff89d57e614209ebe199d.png"
+  },
+  {
+    "url": "/icon_512x512.9d6dce861e3a4ad0ebe214c6737067c8.png"
+  },
+  {
+    "url": "/icon_96x96.ea03be1dfe8e15ac9031c6c8c7772b31.png"
+  },
+  {
+    "revision": "f581d6966222d082b1da5db70dcf6a5c",
+    "url": "/index.html"
+  },
+  {
+    "url": "/manifest.9520d2d663bef8f830fa1cf326995a99.json"
+  }
+]);
