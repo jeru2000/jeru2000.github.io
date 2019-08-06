7cfd51c23ca2efb5ab6259ce3cc2e8b30c287b54
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-08-06 14:27:54 -0400

    Se cambio el cambio de idioma y se agrego el envio de formulario

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.404dcffd5148d3b239a047124e491e50.js"
+  "/precache-manifest.58bb063244b7eca36912145b488304d2.js"
 );
 
 self.addEventListener('message', (event) => {
