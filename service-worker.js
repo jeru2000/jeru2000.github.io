0fd48f7aa1f350ea3810359c8f54af019f9c5322
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-08-08 10:45:52 -0400

    Se arreglo el responsive

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8962d98f27632eda32492891b1ca9c30.js"
+  "/precache-manifest.09c8718b6ef4d3346a4391b5f19747c5.js"
 );
 
 self.addEventListener('message', (event) => {
