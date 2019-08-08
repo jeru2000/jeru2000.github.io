4c23e39980f2be8001dca2430f6f8cdf0937fa18
jesusrojas-frontend <jesusrojas.frontend@gmail.com>
2019-08-08 09:45:30 -0400

    Se agregaron los link de las redes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48dda856be494d787ca385528eb396c8.js"
+  "/precache-manifest.8962d98f27632eda32492891b1ca9c30.js"
 );
 
 self.addEventListener('message', (event) => {
